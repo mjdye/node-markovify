bca786ddf1748308d082c834ab3094b9369ed74e
Matt <mattdye3@gmail.com>
2017-09-28 10:11:04 -0400

    Removing twitter dependency

--- examples/from-twitter.js
+++ examples/from-twitter.js
@@ -1,28 +1,19 @@
 var markovTwitter = require('../lib/markov-twitter.js');
-var consumer_key = "your_consumer_key_here";
-var consumer_secret = "your_consumer_secret_here";
-var access_token_key = "your_access_token_here";
-var access_token_secret = "your_access_token_secret_here";
 var useLocalTweets = false;
-var filters = ' -filter:retweets -filter:media';
 var state_size = 2;
 
 var options = {
-    consumer_key:consumer_key,
-    consumer_secret:consumer_secret,
-    access_token_key:access_token_key,
-    access_token_secret:access_token_secret,
-    getLocalTweets:useLocalTweets,
     state_size:state_size,
-    search_string: '#takeaknee' + filters,
-    numTweetsToFetch: 100,
-    numTweetsToPredict: 10
+    numTweetsToPredict: 2
 }
 
 var thisMarkovTwitter = new markovTwitter(options);
-thisMarkovTwitter.getTweets(options,function(tweets){
+var tweets = [
+    'There was a young man From Cork who got limericks And haikus confused #NationalPoetryDay',
+    'O, yes, I say it plain, America never was America to me, And yet I swear this oath—America will be! - Langston Hughes #NationalPoetryDay',
+    'War\'s a horrid thing, by S. Baldrick Hear the words I sing War\'s a horrid thing So I sing sing sing ding-a-ling-a-ling #NationalPoetryDay'
+]
+options.tweets = tweets;
+thisMarkovTwitter.generateMarkovTweets(options,function(tweets){
     console.log(tweets)
 });
-
-
-
